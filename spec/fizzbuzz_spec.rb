88221f716ee15110679f7d87893b9d671eed25fc
BenDunjay <bendunjay@outlook.com>
2020-11-16 15:56:22 +0000

    split out the logic method and changed the tests so they can be tested with a single number

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -14,23 +14,23 @@ RSpec.describe FizzBuzz do
 
   describe "#fizzbuzz_action method" do
     context "is divisible by 3" do
-      let(:divisible_by_3) { FizzBuzz.new(3) }
+      let(:divisible_by_3) { FizzBuzz.new }
       it "will return `Fizz` if the number is divisible by 3 " do
-        expect(divisible_by_3.fizzbuzz_action).to eq([1, 2, "Fizz"])
+        expect(divisible_by_3.fizzbuzz_output(3)).to eq("Fizz")
       end
     end
 
     context "is divisible by 5" do
-      let(:divisible_by_5) { FizzBuzz.new(5) }
+      let(:divisible_by_5) { FizzBuzz.new }
       it "will return `Fizz` if the number is divisible by 5 " do
-        expect(divisible_by_5.fizzbuzz_action).to eq([1, 2, "Fizz", 4, "Buzz"])
+        expect(divisible_by_5.fizzbuzz_output(5)).to eq("Buzz")
       end
     end
 
     context "is divisible by 3 and 5" do
-      let(:divisible_by_3_and_5) { FizzBuzz.new(15) }
+      let(:divisible_by_3_and_5) { FizzBuzz.new }
       it "will return `Fizz` if the number is divisible by 3 and 5 " do
-        expect(divisible_by_3_and_5.fizzbuzz_action).to eq([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"])
+        expect(divisible_by_3_and_5.fizzbuzz_output(15)).to eq("FizzBuzz")
       end
     end
   end # fizzbuzz method describe end
