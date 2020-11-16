951f8f13acc6c6c9eb5a6e750d8caa5b5558e253
BenDunjay <bendunjay@outlook.com>
2020-11-16 15:16:40 +0000

    tests written for FizzBuzz fun

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,26 @@
+class FizzBuzz
+  def initialize(n)
+    @number = n
+  end
+
+  def fizzbuzz_action
+    new_array = []
+    numbers = 1..@number.to_i
+    numbers.each do |digit|
+      divisble_by_3 = digit % 3 == 0
+      divisble_by_5 = digit % 5 == 0
+
+      if divisble_by_3 && divisble_by_5
+        new_array << "FizzBuzz"
+      elsif divisble_by_3
+        new_array << "Fizz"
+      elsif divisble_by_5
+        new_array << "Buzz"
+      else
+        new_array << digit
+      end
+    end
+
+    new_array
+  end
+end #class end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,41 @@
+require "./lib/fizzbuzz.rb"
+
+RSpec.describe FizzBuzz do
+  let(:numbers) { described_class.new(15) }
+  context "initializing" do
+    it "creates a new instance" do
+      expect(numbers).to be_instance_of(FizzBuzz)
+    end
+
+    it " works with no argument" do
+      expect(FizzBuzz).to respond_to(:new).with(1).arguments
+    end
+
+    it "does not accept arguments" do
+      expect(FizzBuzz).not_to respond_to(:new).with(0).argument
+    end
+  end
+
+  describe "#fizzbuzz_action method" do
+    context "is divisible by 3" do
+      let(:divisible_by_3) { FizzBuzz.new(3) }
+      it "will return `Fizz` if the number is divisible by 3 " do
+        expect(divisible_by_3.fizzbuzz_action).to eq([1, 2, "Fizz"])
+      end
+    end
+
+    context "is divisible by 5" do
+      let(:divisible_by_5) { FizzBuzz.new(5) }
+      it "will return `Fizz` if the number is divisible by 3 " do
+        expect(divisible_by_5.fizzbuzz_action).to eq([1, 2, "Fizz", 4, "Buzz"])
+      end
+    end
+
+    context "is divisible by 3 and 5" do
+      let(:divisible_by_3_and_5) { FizzBuzz.new(15) }
+      it "will return `Fizz` if the number is divisible by 3 " do
+        expect(divisible_by_3_and_5.fizzbuzz_action).to eq([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"])
+      end
+    end
+  end # fizzbuzz method describe end
+end #class end
